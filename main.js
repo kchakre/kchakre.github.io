f543ed83ed82856725a87935d219c67a8a598843
kishor <kishor@ubuntu.(none)>
2014-02-04 21:52:38 +0530

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 	function calc(){
 		var num1,num2,result;
-		num1 = parseInt(document.form1.txt1.value);
-		num2 = parseInt(document.form1.txt2.value);
+		num1 = parseFloat(document.form1.txt1.value);
+		num2 = parseFloat(document.form1.txt2.value);
 
 		result = (num1*num2/100);
 
