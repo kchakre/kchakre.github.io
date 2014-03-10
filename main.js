19a9272b977093df35bc615afe0a8c4f4651ffca
kishor <kishor@ubuntu.(none)>
2014-03-10 21:30:20 +0530

    re

--- main.js
+++
@@ -1,9 +0,0 @@
-	function calc(){
-		var num1,num2,result;
-		num1 = parseFloat(document.form1.txt1.value);
-		num2 = parseFloat(document.form1.txt2.value);
-
-		result = (num1*num2/100);
-
-		alert("RESULT: "+result);
-	}
